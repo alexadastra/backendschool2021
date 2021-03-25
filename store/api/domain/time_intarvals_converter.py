b150ce38c72c221188135f2c5767f51aa7eb7d07
alexadastra <semenchekoaleksey92@gmail.com>
2021-03-25 17:01:55 +0300

    added PATCH couriers/{id} without orders handling

--- store/api/domain/time_intarvals_converter.py
+++ store/api/domain/time_intarvals_converter.py
@@ -3,7 +3,7 @@
 class TimeIntervalsConverter:
     @staticmethod
     def int_to_string(time):
-        hours = str(int(time / 60))
+        hours = str(int(time / 60)) if time / 60 > 9 else "0" + str(int(time / 60))
         minutes = str(time % 60) if time % 60 > 9 else "0" + str(time % 60)
         return hours + ":" + minutes
 
@@ -26,3 +26,24 @@ class TimeIntervalsConverter:
                 .append(TimeIntervalsConverter
                         .int_to_string_interval(time_start_intervals_unique[i], time_finish_intervals_unique[i]))
         return time_intervals
+
+    @staticmethod
+    def string_to_int(time):
+        spl = time.split(":")
+        hours, minutes = int(spl[0]) , int(spl[1])
+        return hours * 60 + minutes
+
+    @staticmethod
+    def string_to_int_interval(time_interval):
+        spl = time_interval.split("-")
+        return TimeIntervalsConverter.string_to_int(spl[0]), TimeIntervalsConverter.string_to_int(spl[1])
+
+    @staticmethod
+    def string_to_int_array(time_intervals):
+        time_start_intervals = []
+        time_finish_intervals = []
+        for time_interval in time_intervals:
+            time_start, time_finish = TimeIntervalsConverter.string_to_int_interval(time_interval)
+            time_start_intervals.append(time_start)
+            time_finish_intervals.append(time_finish)
+        return time_start_intervals, time_finish_intervals
